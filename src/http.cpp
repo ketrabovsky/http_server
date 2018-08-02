d2d60579b45cbe7fb286f0ab465df8c94a32ac35
ketrabovsky <ktr@DESKTOP-01M0E6N.home>
2018-08-02 10:17:41 +0200

    Refactored status codes map initialization.
    Added correct implementation of Http::setStatusCode(int)

--- src/http.cpp
+++ src/http.cpp
@@ -1,51 +1,57 @@
 #include "http.hpp"
 
-namespace Http {
-	std::map <int, std::string> codes;
+#include <string>
 
+namespace Http {
+	const std::string CRLF = "\r\n";
+
+	std::map <int, std::string> codes {
+			{CONTINUE, "Continue"},
+			{SWITCHING_PROTOCOLS, "Switching Protocols"},
+			{OK, "OK"},
+			{CREATED, "Created"},
+			{ACCEPTED, "Accepted"},
+			{NON_AUTHORATIVE_INFORMATION, "Non Authorative Information"},
+			{NO_CONTENT, "No Content"},
+			{RESET_CONTENT, "Reset Content"},
+			{PARTIAL_CONTENT, "Partial Content"},
+			{MULTIPLE_CHOICES, "Multiple Choices"},
+			{MOVED_PERMAMENTLY, "Moved Permamently"},
+			{FOUND, "Found"},
+			{SEE_OTHER, "See Other"},
+			{NOT_MODIFIED, "Not Modified"},
+			{USE_PROXY, "Use Proxy"},
+			{TEMPORARY_REDIRECT, "Temporary Redirect"},
+			{BAD_REQUEST, "Bad Request"},
+			{UNAUTHORIZED, "Unauthorized"},
+			{PAYMENT_REQUIRED, "Payment Required"},
+			{FORBIDDEN, "Forbidden"},
+			{NOT_FOUND, "Not Found"},
+			{METHOD_NOT_ALLOWED, "Method Not Allowed"},
+			{NOT_ACCEPTABLE, "Not Acceptable"},
+			{PROXY_AUTHENTICATION_REQUIRED, "Proxy Authentication Required"},
+			{REQUEST_TIMEOUT, "Request Timeout"},
+			{CONFLICT, "Conflict"},
+			{GONE, "Gone"},
+			{LENGTH_REQUIRED, "Length Required"},
+			{PRECONDITION_FAILED, "Precondition Failed"},
+			{REQUEST_ENTITY_TOO_LARGE, "Request Entity Too Large"},
+			{REQUEST_URI_TOO_LONG, "Request Uri Too Long"},
+			{UNSUPPORTED_MEDIA_TIME, "Unsupported Media Time"},
+			{REQUESTED_RANGE_NOT_SATIFSFIABLE, "Requested Range Not Satifsfiable"},
+			{EXPECTATION_FAILED, "Expectation Failed"},
+			{INTERNAL_SERVER_ERROR, "Internal Server Error"},
+			{NOT_IMPLEMENTED, "Not Implemented"},
+			{BAD_GATEWAY, "Bad Gateway"},
+			{SERVICE_UNAVAILABLE, "Service Unavailable"},
+			{GATEWAY_TIMEOUT, "Gateway Timeout"},
+			{HTTP_VERSION_NOT_SUPPORTED, "Http Version Not Supported"},
+	};
 
 	Http::Http()
 	{
-		codes[CONTINUE] = "Continue";
-		codes[SWITCHING_PROTOCOLS] = "Switching Protocols";
-		codes[OK] = "Ok";
-		codes[CREATED] = "Created";
-		codes[ACCEPTED] = "Accepted";
-		codes[NON_AUTHORATIVE_INFORMATION] = "Non Authorative Information";
-		codes[NO_CONTENT] = "No Content";
-		codes[RESET_CONTENT] = "Reset Content";
-		codes[PARTIAL_CONTENT] = "Partial Content";
-		codes[MULTIPLE_CHOICES] = "Multiple Choices";
-		codes[MOVED_PERMAMENTLY] = "Moved Permamently";
-		codes[FOUND] = "Found";
-		codes[SEE_OTHER] = "See Other";
-		codes[NOT_MODIFIED] = "Not Modified";
-		codes[USE_PROXY] = "Use Proxy";
-		codes[TEMPORARY_REDIRECT] = "Temporary Redirect";
-		codes[BAD_REQUEST] = "Bad Request";
-		codes[UNAUTHORIZED] = "Unauthorized";
-		codes[PAYMENT_REQUIRED] = "Payment Required"; codes[FORBIDDEN] = "Forbidden";
-		codes[NOT_FOUND] = "Not Found";
-		codes[METHOD_NOT_ALLOWED] = "Method Not Allowed";
-		codes[NOT_ACCEPTABLE] = "Not Acceptable";
-		codes[PROXY_AUTHENTICATION_REQUIRED] = "Proxy Authentication Required";
-		codes[REQUEST_TIMEOUT] = "Request Timeout";
-		codes[CONFLICT] = "Conflict";
-		codes[GONE] = "Gone";
-		codes[LENGTH_REQUIRED] = "Length Required";
-		codes[PRECONDITION_FAILED] = "Precondition Failed";
-		codes[REQUEST_ENTITY_TOO_LARGE] = "Request Entity Too Large";
-		codes[REQUEST_URI_TOO_LONG] = "Request Uri Too Long";
-		codes[UNSUPPORTED_MEDIA_TIME] = "Unsupported Media Time";
-		codes[REQUESTED_RANGE_NOT_SATIFSFIABLE] = "Requested Range Not Satifsfiable";
-		codes[EXPECTATION_FAILED] = "Expectation Failed";
-		codes[INTERNAL_SERVER_ERROR] = "Internal Server Error";
-		codes[NOT_IMPLEMENTED] = "Not Implemented";
-		codes[BAD_GATEWAY] = "Bad Gateway";
-		codes[SERVICE_UNAVAILABLE] = "Service Unavailable";
-		codes[GATEWAY_TIMEOUT] = "Gateway Timeout";
-		codes[HTTP_VERSION_NOT_SUPPORTED] = "Http Version Not Supported";
 	}
+
 	
 	Http::~Http()
 	{
@@ -63,6 +69,7 @@ namespace Http {
 
 	void Http::setStatusCode(int status)
 	{
+		this->status = status;
 	}
 
 	void Http::setResponseBody(const std::string &body)
@@ -71,6 +78,7 @@ namespace Http {
 
 	bool Http::parseHttp(const std::string &httpRequest)
 	{
+		return false;
 	}
 
 	std::map <std::string, std::string> Http::getTokens() const
